839aab91dbcffb55fa4c6e15fd2ef07ff29201df
Shahnawaz Memon <shahnawaz.satyainfosys@gmail.com>
2022-10-17 19:56:05 +0530

    Add Dashboard route - Add default avatar image

---
+++ src/routes/routes.js
@@ -0,0 +1,14 @@
+export const dashboardRoutes = {
+  label: 'Dashboard',
+  labelDisable: true,
+  children: [
+    {
+      name: 'Dashboard',
+      icon: 'chart-pie',
+      to: '/dashboard',
+      active: true
+    },
+  ]
+};
+
+export default [dashboardRoutes];
