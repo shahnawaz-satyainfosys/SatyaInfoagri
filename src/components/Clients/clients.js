6810f32e428a196521f97fda528fbb2590dc457f
Shahnawaz Memon <shahnawaz.satyainfosys@gmail.com>
2022-11-02 11:06:19 +0530

    Pass tab names from Clients component to TagPage

--- src/components/Clients/clients.js
+++ src/components/Clients/clients.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import TabPage from 'components/common/TabPage';
 
+const tabList = {
+  tab1: "Customer List",
+  tab2: "Customer Details",
+  tab3: "Transaction Details"
+};
+
 const Client = () => {
   return (
     <>
-      <TabPage />
+      <TabPage tab={tabList} />
     </>
   );
 };
