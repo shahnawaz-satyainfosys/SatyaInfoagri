776fedc5b7338491e6f9cf2f843e49bac94e0743
Shahnawaz Memon <shahnawaz.satyainfosys@gmail.com>
2022-12-22 00:10:30 +0530

    Fix footer spacing and open links in new tab

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,11 +6,7 @@ const Footer = () => (
   <footer className="footer">
     <Row className="justify-content-between fs--1 mt-2 mb-2 footer-credits">
       <Col sm="auto">
-        <p className="mb-0 text-600">
-        | <a href="http://khalihan.in">Khalihan</a> | 
-          <a href="http://satyainfosys.com">Satya Infosys</a> |
-          {new Date().getFullYear()} &copy; all right reserved |
-          <span className="mb-0 text-600"> Version {version} |</span>
+        <p className="mb-0 text-600">| <a href="http://khalihan.in" target="_blank">Khalihan</a> | <a href="http://satyainfosys.com" target="_blank">Satya Infosys</a> | {new Date().getFullYear()} &copy; all right reserved | <span className="mb-0 text-600"> Version {version} |</span>
         </p>
       </Col>
       <Col sm="auto"></Col>
